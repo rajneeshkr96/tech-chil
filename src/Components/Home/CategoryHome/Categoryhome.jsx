d73488dafdbd3485efc45eac096acb80f26c681e
Rajneesh <rajneeshkumar9674737@gmail.com>
2023-09-13 15:56:59 +0530

    fixing home category

--- src/Components/Home/CategoryHome/Categoryhome.jsx
+++ src/Components/Home/CategoryHome/Categoryhome.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import "./Categoryhome.css"
 import catgaimg from "../../../../public/CateHeadphone.png"
 function Categoryhome({category}) {
-    const headphone = "headphone";
-    console.log(category)
   return (
    <section>
     <div className='categorys'>
  {   category.map((category) =><div className='category'>
-            <p>{category}</p>
+            <span>{category}</span>
+            <div className='ctg-img'>
             <img className='cate-img' src={catgaimg} alt="" />
+            </div>
         </div>)}
         
 
